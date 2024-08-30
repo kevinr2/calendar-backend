0623a2cb776ffe4533bb80c6dab50b2549810dc8
kevinr2 <kevinreve096@gmail.com>
2024-08-29 21:38:02 -0500

    s

--- index.js
+++ index.js
@@ -24,9 +24,9 @@ app.use(express.json())
 app.use('/api/auth',require('./routes/auth'))
 app.use('/api/events',require('./routes/events'))
 
-app.use('*', (req,res)=>{
+/* app.use('*', (req,res)=>{
     res.sendFile(path.join(__dirname,'public/index.html'))
-})
+}) */
 
 
 
